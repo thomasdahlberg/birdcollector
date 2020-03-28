a8fe4e09df358c9fd9388b8a525f654df8662791
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-03-28 17:21:45 -0400

    implements feeding datatype

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bird, Feeding
 
 # Register your models here.
+
+admin.site.register(Bird)
+admin.site.register(Feeding)
+
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+FOODS = (
+    ('M', 'Millet'),
+    ('S', 'Suet')
+)
+
 # Create your models here.
 class Bird(models.Model):
     sitename = models.CharField(max_length=100)
@@ -13,3 +18,15 @@ class Bird(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'bird_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    food = models.CharField(
+        max_length=1,
+        choices=FOODS,
+        default=FOODS[0][0]
+    )
+    bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_food_display()} on {self.date}"
