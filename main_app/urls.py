d10517fb9bea1b1b9e8cd408be40ae24b9ad1d48
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-04-04 16:56:25 -0400

    implements assoc_gift functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('birds/<int:pk>/update/', views.BirdUpdate.as_view(), name='bird_update'),
     path('birds/<int:pk>/delete/', views.BirdDelete.as_view(), name='bird_delete'),
     path('birds/<int:bird_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    path('birds/<int:bird_id>/assoc_gift/<int:gift_id>/', views.assoc_gift, name='assoc_gift'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Bird
+from .models import Bird, Gift
 from .forms import FeedingForm
 
 
@@ -17,10 +17,12 @@ def birds_index(request):
 
 def bird_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
+    unassigned_gifts = Gift.objects.exclude(id__in = bird.gifts.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'birds/detail.html', {
         'bird': bird, 
-        'feeding_form': feeding_form
+        'feeding_form': feeding_form,
+        'gifts': unassigned_gifts
     })
 
 def add_feeding(request, bird_id):
@@ -29,6 +31,10 @@ def add_feeding(request, bird_id):
         new_feeding = form.save(commit=False)
         new_feeding.bird_id = bird_id
         new_feeding.save()
+    return redirect('detail', bird_id=bird_id)
+
+def assoc_gift(request, bird_id, gift_id):
+    Bird.objects.get(id=bird_id).gifts.add(gift_id)
     return redirect('detail', bird_id=bird_id)
 
 class BirdCreate(CreateView):
